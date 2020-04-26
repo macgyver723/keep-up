11bacfd1a5b2f14115db1995a73b8d634ae9fd86
Stefan <stefanbfritz@gmail.com>
2020-04-26 07:42:01 -0400

    cleaned up routes a bit

--- app.py
+++ app.py
@@ -11,7 +11,42 @@ auth0 = setup_auth(app)
 
 @app.route('/')
 def index():
-    return "not implemented"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL)
+
+'''
+Login --> callback /interactions
+    attempt to access page without login --> Back to login
+
+/interactions:
+    GET 
+        - returns all interactions ordred by date
+        - returns list of contact names (not a priority)
+        - paginate to help with loading
+    POST
+        - search
+            - query interactions db inside of notes column
+        - add contact
+            - get form args
+                - contact name
+                - contact frequency (this will need to be some sort of popup or something)
+        - add interaction
+            - get form args
+                - contact name
+                - timestamp
+                - method
+                - duration
+                - notes (multi-line --> use <textarea> in the <form> also use javascript to alter dimensions of area)
+            - find contact id
+            - use user id
+            - add user to session
+            
+
+
+'''
 
 @app.route('/callback')
 def callback_handling():
@@ -33,17 +68,12 @@ def callback_handling():
     #     "profile": jsonify(session['profile'])
     # })
     
-    return redirect('/dashboard')
-
-@app.route('/login')
-def login():
-    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL)
+    # return redirect('/dashboard')
+    return redirect(url_for('interactions'))
 
 @app.route('/dashboard')
 @requires_auth
 def dashboard():
-    print(f"\tuser_id: {session['profile']['user_id']}")
-    print(f"\tuser's name: {session['profile']['name']}")
     return render_template('dashboard.html',
                            userinfo=session['profile'],
                            userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
@@ -54,13 +84,9 @@ def logout():
     session.clear()
     # Redirect user to logout endpoint
     params = {'returnTo': url_for('index', _external=True), 'client_id': AUTH0_CLIENT_ID}
-    print(f"params: {params}")
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 @app.route('/interactions')
 @requires_auth
-def get_interactions():
-    return jsonify({
-        "success" : True,
-        "user" : session['profile']
-    })
+def interactions():
+    return render_template('interactions.html')
